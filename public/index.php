ec73c5acfff6052de46d94823e33aa9e13861b83
yukyhendiawan <yukyhendiawan123@gmail.com>
2025-04-19 23:09:33 +0700

    refactor: index.php file

--- public/index.php
+++ public/index.php
@@ -1,1 +1,13 @@
-<?php // Silence is golden
+<?php
+/**
+ * The index.php file.
+ *
+ * This file is intentionally left blank to prevent direct access to the directory.
+ *
+ * @link       https://yukyhendiawan.com
+ * @since      1.0.0
+ *
+ * @package    Icon_Font_Awesome
+ */
+
+// Silence is golden.
